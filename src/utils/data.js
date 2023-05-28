b00366d0952d04f0b43f24db986be923b20f191c
belaekaputri <belaekaputri28@gmail.com>
2023-05-29 01:20:40 +0700

    add file

---
+++ src/utils/data.js
@@ -0,0 +1,23 @@
+const getData = () => {
+  return [
+    {
+       id:1,
+       name: 'Kim Hanbin',
+       tag : 'bi',
+       imageUrl : '/images/1.jpg',
+    },
+    {
+        id:2,
+        name: 'Bobby',
+        tag : 'by',
+        imageUrl : '/images/2.jpg',
+     },
+     {
+        id:3,
+        name: 'Nam Dosan',
+        tag : 'dsn',
+        imageUrl : '/images/3.jpg',
+     },
+  ];
+}
+export { getData };
