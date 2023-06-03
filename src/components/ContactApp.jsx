fe76299a33f312e61b5a4f6e95362c0265b12236
belaekaputri <belaekaputri28@gmail.com>
2023-06-03 12:04:43 +0700

    tambah form

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ContactList from './ContactList';
 import { getData } from '../utils/data';
+import ContactInput from './ContactInput';
  
-class ContactApp extends React.Component {
+class ContactApp extends React.Component { //class component
   constructor(props) {
     super(props);
     this.state = {
@@ -10,16 +11,36 @@ class ContactApp extends React.Component {
     }
   
     this.onDeleteHandler = this.onDeleteHandler.bind(this);
+    this.onAddContactHandler = this.onAddContactHandler.bind(this);
   }
   
   onDeleteHandler(id) {
     const contacts = this.state.contacts.filter(contact => contact.id !== id);
     this.setState({ contacts });
   }
+  onAddContactHandler({ name, tag }){
+    this.setState(( prevState )=>{
+      return{
+        contacts: [
+          ...prevState.contacts,
+          {
+            id : +new Date(),
+            name,
+            tag,
+            imageUrl: '/images/2.jpg',
+          }
+        ]
+      }
+    });
+  }
   
   render() {
     return (
       <div className="contact-app">
+       <h1>Aplikasi Kontak</h1>
+       <h2>Tambah Kontak</h2>
+       <ContactInput addContact={this.onAddContactHandler} />
+
         <h1>Daftar Kontak</h1>
         <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
       </div>
